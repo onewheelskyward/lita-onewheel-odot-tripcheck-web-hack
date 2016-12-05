d5ce1ed13cd8e01889649cc7e848c429e89799c5
Andrew Kreps <andrew.kreps@gmail.com>
2016-12-05 15:23:49 -0800

    No-op refactor.

--- lib/lita/handlers/onewheel_odot_tripcheck_web_hack.rb
+++ lib/lita/handlers/onewheel_odot_tripcheck_web_hack.rb
@@ -9,16 +9,18 @@ module Lita
             help: { 'tripcheck': 'Display a list of tripcheck cameras covered.'})
 
       def get_cameras
+        uri_base = 'https://tripcheck.com/RoadCams/cams/'
+
         {
           # 'Hood River': %w(Hood%20River%20Exit%2064a_pid1863.JPG Hood%20River%20Exit%2063_pid1860.JPG),
-          'Hood River': 'Hood%20River%20Exit%2064a_pid1863.JPG',
-          '26 at 35': 'US26%20at%20Hwy35_pid1881.JPG',
-          '26 at Snow Bunny': 'US26%20at%20Snow%20Bunny_pid1887.JPG',
+          'Hood River': uri_base + 'Hood%20River%20Exit%2064a_pid1863.JPG',
+          '26 at 35': uri_base + 'US26%20at%20Hwy35_pid1881.JPG',
+          '26 at Snow Bunny': uri_base + 'US26%20at%20Snow%20Bunny_pid1887.JPG',
           # '26 at Govy': %w(GovernmentCamp_pid624.jpg US26%20at%20Govn%20Camp%20Maint_pid1899.JPG),
-          '26 at Govy': 'US26%20at%20Govn%20Camp%20Maint_pid1899.JPG',
-          '26 at Govy Loop Rd': 'US26%20at%20Govt%20Camp%20Lp%20Rd_pid1776.JPG',
-          '26 at Frog Lake': 'US26%20at%20Frog%20Lake%20VMS_pid1773.JPG',
-          '26 at Blue Box Pass': 'Blue%20Box%20Pass_pid1919.JPG'
+          '26 at Govy': uri_base + 'US26%20at%20Govn%20Camp%20Maint_pid1899.JPG',
+          '26 at Govy Loop Rd': uri_base + 'US26%20at%20Govt%20Camp%20Lp%20Rd_pid1776.JPG',
+          '26 at Frog Lake': uri_base + 'US26%20at%20Frog%20Lake%20VMS_pid1773.JPG',
+          '26 at Blue Box Pass': uri_base + 'Blue%20Box%20Pass_pid1919.JPG'
         }
       end
 
@@ -26,12 +28,10 @@ module Lita
         input = response.matches[0][0]
         Lita.logger.debug "Looking for '#{input}'"
 
-        uri_base = 'https://tripcheck.com/RoadCams/cams/'
-
         get_cameras.keys.each do |camera_key|
           Lita.logger.debug "Trying to match '#{input.downcase}' to '#{camera_key.downcase}'"
           if camera_key.to_s.downcase.include? input.downcase
-            response.reply "#{uri_base}#{get_cameras[camera_key]}?rand=#{Time.now.to_i}123"
+            response.reply "#{get_cameras[camera_key]}?rand=#{Time.now.to_i}123"
             return   # Quick exit.
           end
         end

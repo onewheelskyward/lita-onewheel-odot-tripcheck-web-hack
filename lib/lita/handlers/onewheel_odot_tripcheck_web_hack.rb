d374ce92b72b3755aae02390bb3cd84f0613d17e
Andrew Kreps <andrew.kreps@gmail.com>
2016-12-05 15:15:44 -0800

    Added a helpful list.

--- lib/lita/handlers/onewheel_odot_tripcheck_web_hack.rb
+++ lib/lita/handlers/onewheel_odot_tripcheck_web_hack.rb
@@ -5,30 +5,41 @@ module Lita
       route(/^tripcheck\s+(.*)/i, :handle_tripcheck, command: true,
             help: { 'tripcheck 35': 'Displays an image of hwy 26 @ hwy 35'})
 
+      route(/^tripcheck$/i, :handle_tripcheck_list, command: true,
+            help: { 'tripcheck': 'Display a list of tripcheck cameras covered.'})
+
+      def get_cameras
+        {
+          # 'Hood River': %w(Hood%20River%20Exit%2064a_pid1863.JPG Hood%20River%20Exit%2063_pid1860.JPG),
+          'Hood River': 'Hood%20River%20Exit%2064a_pid1863.JPG',
+          '26 at 35': 'US26%20at%20Hwy35_pid1881.JPG',
+          '26 at Snow Bunny': 'US26%20at%20Snow%20Bunny_pid1887.JPG',
+          # '26 at Govy': %w(GovernmentCamp_pid624.jpg US26%20at%20Govn%20Camp%20Maint_pid1899.JPG),
+          '26 at Govy': 'US26%20at%20Govn%20Camp%20Maint_pid1899.JPG',
+          '26 at Govy Loop Rd': 'US26%20at%20Govt%20Camp%20Lp%20Rd_pid1776.JPG',
+          '26 at Frog Lake': 'US26%20at%20Frog%20Lake%20VMS_pid1773.JPG',
+          '26 at Blue Box Pass': 'Blue%20Box%20Pass_pid1919.JPG'
+        }
+      end
+
       def handle_tripcheck(response)
         input = response.matches[0][0]
         Lita.logger.debug "Looking for '#{input}'"
 
         uri_base = 'https://tripcheck.com/RoadCams/cams/'
-        cameras = {
-            # 'Hood River': %w(Hood%20River%20Exit%2064a_pid1863.JPG Hood%20River%20Exit%2063_pid1860.JPG),
-            'Hood River': 'Hood%20River%20Exit%2064a_pid1863.JPG',
-            '26 at 35': 'US26%20at%20Hwy35_pid1881.JPG',
-            '26 at Snow Bunny': 'US26%20at%20Snow%20Bunny_pid1887.JPG',
-            # '26 at Govy': %w(GovernmentCamp_pid624.jpg US26%20at%20Govn%20Camp%20Maint_pid1899.JPG),
-            '26 at Govy': 'US26%20at%20Govn%20Camp%20Maint_pid1899.JPG',
-            '26 at Govy Loop Rd': 'US26%20at%20Govt%20Camp%20Lp%20Rd_pid1776.JPG',
-            '26 at Frog Lake': 'US26%20at%20Frog%20Lake%20VMS_pid1773.JPG',
-            '26 at Blue Box Pass': 'Blue%20Box%20Pass_pid1919.JPG'
-        }
 
-        cameras.keys.each do |camera_key|
+        get_cameras.keys.each do |camera_key|
           Lita.logger.debug "Trying to match '#{input.downcase}' to '#{camera_key.downcase}'"
           if camera_key.to_s.downcase.include? input.downcase
-            response.reply "#{uri_base}#{cameras[camera_key]}?rand=#{Time.now.to_i}123"
+            response.reply "#{uri_base}#{get_cameras[camera_key]}?rand=#{Time.now.to_i}123"
             return   # Quick exit.
           end
         end
+      end
+
+      def handle_tripcheck_list(response)
+        Lita.logger.debug "Replying with: Cameras available include: #{get_cameras.keys.join ', '}"
+        response.reply "Cameras available include: #{get_cameras.keys.join ', '}"
       end
 
     end
--- spec/lita/handlers/onewheel_odot_tripcheck_web_hack_spec.rb
+++ spec/lita/handlers/onewheel_odot_tripcheck_web_hack_spec.rb
@@ -5,9 +5,15 @@ describe Lita::Handlers::OnewheelOdotTripcheckWebHack, lita_handler: true do
   end
 
   it { is_expected.to route_command('tripcheck blue') }
+  it { is_expected.to route_command('tripcheck') }
 
   it 'will respond to tripcheck' do
     send_command 'tripcheck govy'
     expect(replies.last).to include('https://tripcheck.com/RoadCams/cams/US26%20at%20Govn%20Camp%20Maint_pid1899.JPG')
+  end
+
+  it 'will list tripchecks' do
+    send_command 'tripcheck'
+    expect(replies.last).to eq('Cameras available include: Hood River, 26 at 35, 26 at Snow Bunny, 26 at Govy, 26 at Govy Loop Rd, 26 at Frog Lake, 26 at Blue Box Pass')
   end
 end
